c94c13b268ebb3c2d025a39597ab6c85fb225a6f
flowtoolz <contact@flowtoolz.com>
2021-08-22 13:10:23 +0200

    Add SwiftUI experiments

--- Code/SwiftUIToolz/SwiftUI+SwiftObserver.swift
+++ Code/SwiftUIToolz/SwiftUI+SwiftObserver.swift
@@ -5,12 +5,12 @@ import SwiftObserver
 
 public extension View {
     func bind<O: ObservableCache>(_ binding: Binding<O.Message>,
-                                         to observable: O) -> some View {
+                                  to observable: O) -> some View {
         bind(binding, to: observable.publisher())
     }
     
-    func bind<O: Observable>(_ binding: Binding<O.Message>,
-                                    to observable: O) -> some View {
+    func bind<O: SwiftObserver.ObservableObject>(_ binding: Binding<O.Message>,
+                             to observable: O) -> some View {
         bind(binding, to: observable.publisher())
     }
 }
