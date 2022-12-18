477f4e7cb7f5551deacb7389813a37993fdad004
Sebastian Fichtner <contact@flowtoolz.com>
2022-12-18 02:00:04 +0100

    Adjust NSApp toggles yet again

--- Code/App+Toggles.swift
+++ Code/App+Toggles.swift
@@ -1,20 +1,21 @@
 #if os(macOS)
 import SwiftUI
 
-@available(macOS 11.0, *)
-public extension App
+public extension NSApplication
 {
-    static func toggleSidebar()
+    /// Just call this on `NSApp`
+    func toggleSidebar()
     {
         // https://stackoverflow.com/questions/61771591/toggle-sidebar-in-swiftui-navigationview-on-macos
-        NSApp.sendAction(#selector(NSSplitViewController.toggleSidebar(_:)),
-                         to: nil,
-                         from: nil)
+        sendAction(#selector(NSSplitViewController.toggleSidebar(_:)),
+                   to: nil,
+                   from: nil)
     }
     
-    static func toggleFullscreen()
+    /// Just call this on `NSApp`
+    func toggleFullscreen()
     {
-        NSApp.keyWindow?.toggleFullScreen(nil)
+        keyWindow?.toggleFullScreen(nil)
     }
 }
 #endif
